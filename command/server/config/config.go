f676acfbe9e8ebcf5b266ffd044e4c4dababa5de
topdefi <94203269+topdefi@users.noreply.github.com>
2022-10-17 10:43:44 -0700

    From 4 to 40 GWEI base

--- command/server/config/config.go
+++ command/server/config/config.go
@@ -97,7 +97,7 @@ func DefaultConfig() *Config {
 		Telemetry:  &Telemetry{},
 		ShouldSeal: true,
 		TxPool: &TxPool{
-			PriceLimit:         4000000000,
+			PriceLimit:         40000000000,
 			MaxSlots:           4096,
 			MaxAccountEnqueued: 128,
 		},
